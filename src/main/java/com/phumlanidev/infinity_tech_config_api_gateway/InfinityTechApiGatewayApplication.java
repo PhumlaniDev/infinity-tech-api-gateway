78f636669be59009994c6937a3e433d6e43f9388
Phumlani Arendse <aphumlani.dev@gmail.com>
2025-04-09 13:30:02 +0200

    rename application name

--- src/main/java/com/phumlanidev/infinity_tech_config_api_gateway/InfinityTechApiGatewayApplication.java
+++ src/main/java/com/phumlanidev/infinity_tech_config_api_gateway/InfinityTechApiGatewayApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class InfinityTechConfigApiGatewayApplication {
+public class InfinityTechApiGatewayApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(InfinityTechConfigApiGatewayApplication.class, args);
+		SpringApplication.run(InfinityTechApiGatewayApplication.class, args);
 	}
 
 }
