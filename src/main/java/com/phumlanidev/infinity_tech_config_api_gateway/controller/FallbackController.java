e683a9a7fed1616f44887d6b7c9863e0f425fd4f
Phumlani Arendse <aphumlani.dev@gmail.com>
2025-04-09 13:19:59 +0200

    added gateway configuration

---
+++ src/main/java/com/phumlanidev/infinity_tech_config_api_gateway/controller/FallbackController.java
@@ -0,0 +1,14 @@
+package com.phumlanidev.infinity_tech_config_api_gateway.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+
+public class FallbackController {
+
+  @GetMapping("/fallback/product")
+  public ResponseEntity<String> fallbackProduct() {
+    return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE)
+        .body("Product Service is currently unavailable. Please try again later.");
+  }
+}
