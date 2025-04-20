4c14f6e84bde85ce7dc21306794b1c1b792986ff
Phumlani Arendse <aphumlani.dev@gmail.com>
2025-04-20 18:04:48 +0200

    update security configuration and enable OAuth2 resource server

--- src/main/java/com/phumlanidev/infinity_tech_config_api_gateway/config/GatewaySecurityConfig.java
+++ src/main/java/com/phumlanidev/infinity_tech_config_api_gateway/config/GatewaySecurityConfig.java
@@ -1,19 +1,29 @@
 package com.phumlanidev.infinity_tech_config_api_gateway.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
-//@EnableWebSecurity
+@EnableWebSecurity
 public class GatewaySecurityConfig {
 
-//  @Bean
-//  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-//    http
-//        .authorizeHttpRequests(auth -> auth
-//            .requestMatchers("/api/auth/**").permitAll()
-//            .anyRequest().authenticated())
-//        .oauth2ResourceServer(oauth2 -> oauth2
-//            .jwt(Customizer.withDefaults()));
-//    return http.build();
-//  }
+  @Bean
+  public SecurityWebFilterChain securityFilterChain(ServerHttpSecurity http) {
+    http
+            .csrf(ServerHttpSecurity.CsrfSpec::disable)
+            .authorizeExchange(exchange -> exchange
+                    .pathMatchers("/actuator/**").permitAll()
+                    .pathMatchers("/v3/api-docs/**").permitAll()
+                    .pathMatchers("/swagger-ui/**").permitAll()
+                    .pathMatchers("/swagger-resources/**").permitAll()
+                    .anyExchange().authenticated()
+            )
+            .oauth2ResourceServer(oauth2 -> oauth2
+            .jwt(Customizer.withDefaults()));
+    return http.build();
+  }
 }
